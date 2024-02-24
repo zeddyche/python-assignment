3e5c1b14294ef008711cf3b062d5d305b2c3baf2
zeddyche <136300564+zeddyche@users.noreply.github.com>
2024-02-24 11:59:11 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,14 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Ask the user for their name
+... name = input("What is your name? ")
+What is your name? Zeddy
+>>> # Ask the user for their age
+... age = input("How old are you? ")
+How old are you? 30
+>>> # Ask the user for their location
+... location = input("Where do you live? ")
+Where do you live? Embu
+>>> # Print out a personalized message
+... print(f"Hello {name}, you are {age} years old and live in {location}.")
+Hello Zeddy, you are 30 years old and live in Embu.
